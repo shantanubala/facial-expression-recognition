d89f3d662184597762c079691e6c8f27534797dc
shantanubala <shantanubala@4ed8b7ad-8181-8302-0958-1d51762c224b>
2010-06-29 17:16:54 +0000

    py-pololu changed

--- trunk/code/py-pololu.py
+++ trunk/code/py-pololu.py
@@ -1,42 +1,142 @@
-import serial
+#!/usr/bin/python
+###########################################################################################
+# Filename: 
+#     micromaestro.py
+###########################################################################################
+# Project Authors: 
+#     Juhapekka Piiroinen
+#     Brian Wu
+# 
+# Changes:
+#     June 14, 2010 by Juhapekka Piiroinen - changes committed to svn
+#           - added comments for the device commands according to the manual from Pololu
+#           - added latest draft code for rotating base servo (Parallax Continuous Rotating Servo)
+#           - note! you should be able to clear error flags with .get_errors function according to the manual
+#           - renamed CameraDriver to LegacyCameraDriver as Brian Wu has done better one
+#           - integrated batch of changes provided by Brian Wu
+#
+#     June 11, 2010 by Brian Wu - Changes committed thru email
+#           - Decoupling the implementation from the program
+#
+#     April 19, 2010 by Juhapekka Piiroinen
+#           - Initial Release
+# 
+# Email:
+#     juhapekka.piiroinen@gmail.com
+#
+# License: 
+#     GNU/GPLv3
+#
+# Description:
+#     A python-wrapper for Pololu Micro Maestro 6-Channel USB Servo Controller
+#
+############################################################################################
+# /!\ Notes /!\
+# You will have to enable _USB Dual Port_ mode from the _Pololu Maestro Control Center_.
+#
+############################################################################################
+# Device Documentation is available @ http://www.pololu.com/docs/pdf/0J40/maestro.pdf
+############################################################################################
+# (C) 2010 Juhapekka Piiroinen
+#          Brian Wu
+############################################################################################
+import Device
+import LegacyCameraDriver
+import CameraDriver
+import time
 import sys
-#set up the serial port for action
-ser=serial.Serial(0)
-ser.baudrate=2400
-def setspeed(n,speed):
-  #Quick check that things are in range
-  if speed > 127 or speed <0:
-    speed=1
-    print "WARNING: Speed should be between 0 and 127. Setting speed to 1..."
-    print "Setting servo "+str(n)+" speed to "+str(speed)+" out of 127."
-  speed=int(speed)
-  #set speed (needs 0x80 as first byte, 0x01 as the second, 0x01 is for speed, 0 for servo 0, and 127 for max speed)
-  bud=chr(0x80)+chr(0x01)+chr(0x01)+chr(n)+chr(speed)
-  ser.write(bud)
 
-def setpos(n,angle):
-  #Check that things are in range
-  if angle > 180 or angle <0:
-    angle=90
-    print "WARNING: Angle range should be between 0 and 180. Setting angle to 90 degrees to be safe..."
-    print "moving servo "+str(n)+" to "+str(angle)+" degrees."
+#############################################################################################
+def test_CameraDriver():
+    print "Test CameraDriver"
+    cd = CameraDriver.CameraDriver()
+    cd.load_file("random.txt")
+    print cd.status()  		# prints out the positions of the servos
+    cd.step()  				# Executes current instruction, moves instruction line by 1
+    print cd.frame_number	# prints out the index of the next instruction
+    print "\n"
 
-  #Valid range is 500-5500
-  offyougo=int(5000*angle/180)+500
-  #Get the lowest 7 bits
-  byteone=offyougo&127
-  #Get the highest 7 bits
-  bytetwo=(offyougo-(offyougo&127))/128
-  #move to an absolute position in 8-bit mode (0x04 for the mode, 0 for the servo, 0-255 for the position (spread over two bytes))
-  bud=chr(0x80)+chr(0x01)+chr(0x04)+chr(n)+chr(bytetwo)+chr(byteone)
-  ser.write(bud)
+    cd.steps(10, True)  			#executes the next 10 steps and print out their directions
+    cd.set_frame(20)  		# sets the index to the 21st command
+    cd.step()					# execute the 20th command
 
-mode=sys.argv[1]
-n=int(sys.argv[2])
-m=int(sys.argv[3])
-if mode=='speed':
-  setspeed(n,m)
-elif mode=='pos':
-  setpos(n,m)
-else:
-  print "No commands given.\nUsage: controlfunction.py speed <servo> <speed
+    cd.shift_frame(-5) 		#adds -5 to the frame index
+    cd.step()					#Executes it
+
+    cd.execute_frame(0) 			# goes to and executes the 0th frame
+    print cd.frame_number	
+
+    cd.execute_procedure([0, 10, 5]) #executes the 0th, then 11th, then 5th frame.  index will then point to the 6th frame
+    print cd.frame_number
+    print cd.status()
+    cd.reset()    			#resets the index to 0
+    cd.unmount()  			#clears the port
+    print "CameraDriver tested"
+    
+#############################################################################################
+def test_LegacyCameraDriver():
+    print "Test Legacy Camera Driver"
+    c = LegacyCameraDriver()
+    print c.status_report()
+    c.pan(50)
+    c.pan(-50)
+    c.tilt(50)
+    c.tilt(-50)
+    ## Rotate function for Parallax Continuous Rotating Servo
+    c.rotate(50) #left
+    time.sleep(0.2)
+    c.rotate(-50) #stop
+    time.sleep(0.2)
+    c.rotate(-50) #right
+    time.sleep(0.2)
+    c.rotate(50) #stop
+    time.sleep(0.2)
+    ## END - Rotate
+    c.reset()
+    del c
+    print "Legacy Camera Driver tested"
+
+#############################################################################################
+def test_Device():
+    print "Test Device"
+    d = Device("COM6","COM7")
+    servos = [0,1]
+    for servo_id in servos:
+        print "-- servo:",servo_id
+        print "Go Home"
+        d.go_home()
+        print "Get Errors"
+        print d.get_errors()
+        
+        print "Get Position"
+        pos = d.get_position(servo_id)
+        print pos        
+        
+        newsetpos = pos-50
+        print "Set Target:",newsetpos
+        d.set_target(servo_id,newsetpos)
+        d.wait_until_at_target()
+        newpos = d.get_position(servo_id)
+        print "%s==%s" % (newsetpos,newpos),
+        print newsetpos==newpos
+        
+        newsetpos = pos+50
+        print "Set Target:",newsetpos
+        d.set_target(servo_id,newsetpos)
+        d.wait_until_at_target()
+        newpos = d.get_position(servo_id)
+        print "%s==%s" % (newsetpos,newpos),
+        print newsetpos==newpos
+        
+        d.go_home()
+        print d.get_errors()
+    del d
+    print "Device tested"
+
+####################################
+if __name__=="__main__":
+    test_Device()
+    # test_CameraDriver()
+    # test_LegacyCameraDriver()
+
+## EOF
